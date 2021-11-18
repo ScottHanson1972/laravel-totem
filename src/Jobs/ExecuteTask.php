c550a07e6d77680654291dee280e964401eba8b2
scotthanson <scott.hanson@mdm3.co.uk>
2021-11-18 10:30:42 +0000

    Check if there is a venv to activate.

--- src/Jobs/ExecuteTask.php
+++ src/Jobs/ExecuteTask.php
@@ -3,12 +3,10 @@
 namespace Studio\Totem\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\DB;
 
 class ExecuteTask implements ShouldQueue
 {
@@ -35,17 +33,23 @@ class ExecuteTask implements ShouldQueue
         // Get the date/time the jobs starts
         $started = microtime(true);
         $startDate = date('m/d/Y h:i:s a', time());
-        $output = exec("cd ".public_path()." && source python/venv/bin/activate && python python/scripts/icecatImporter.py {} {} 2>&1");
+
+        // If there is a venv setup in the python folder we want to use this (Generally used for local install)
+        if (is_dir(base_path().'/python/venv')) {
+            $output = exec("cd ".base_path()." && source python/venv/bin/activate && python python/scripts/icecatImporter.py {} {} 2>&1");
+        }
+        // otherwise assume python is currently configured on the server inline with the system requirements
+        else {
+            $output = exec("cd ".base_path()." && python python/scripts/icecatImporter.py {} {} 2>&1");
+        }
 
         $time_elapsed_secs = microtime(true) - $started;
 
-        DB::table('task_results')->insert([
-            'task_id'   => $this->task->id,
+        // Add a result to the task results table for the task being run
+        $this->task->results()->create([
             'ran_at'    => $startDate,
             'duration'  => $time_elapsed_secs * 1000,
             'result'    => $output,
-            'created_at' => NOW(),
-            'updated_at' => NOW(),
         ]);
     }
 }
